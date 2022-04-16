e783faf4eb7de6e0a924ed61b7cdf5e716df6cb9
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-04-16 21:02:16 +0530

    Update UI

--- assets/main.js
+++ assets/main.js
@@ -22,13 +22,13 @@ const html = document.querySelector('html');
 let isDarkmode = false
 const localDarkmode = JSON.parse(localStorage.getItem('isDarkmode'))
 
-const darkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-</svg>`
-
 const lightIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-</svg>`
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+      </svg>`
+
+const darkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+      </svg>`
 
 
 // Jika ada isDarkmode di localstorage 
@@ -48,11 +48,15 @@ function toggleTheme() {
 function switchTheme() {
     if (isDarkmode) {
         html.classList.add('dark')
+        switchToggle.classList.remove('-translate-x-2')
+        switchToggle.classList.add('translate-x-full')
         setTimeout(() => {
             switchToggle.innerHTML = darkIcon
         }, 250);
     } else {
         html.classList.remove('dark')
+        switchToggle.classList.add('-translate-x-2')
+        switchToggle.classList.remove('translate-x-full')
         setTimeout(() => {
             switchToggle.innerHTML = lightIcon
         }, 250);
