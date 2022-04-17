1a1271bf78cc742b2460e21e2c36729b16cdf6d8
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-04-17 09:25:35 +0530

    UI change

--- assets/main.js
+++ assets/main.js
@@ -65,6 +65,31 @@ function switchTheme() {
 
 switchTheme()
 
+/*==================== Reveal sections ====================*/
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+});
+
+allSections.forEach(function(section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+
+});
+
+
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
